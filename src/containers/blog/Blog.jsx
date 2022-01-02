013ad9b120138134c363b375f70bb7b501bcbe5f
michaWorku <mikias27worku@gmail.com>
2022-01-02 15:08:01 +0300

    blog and article component with styles

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,7 +1,32 @@
-import React from "react";
-import "./blog.css";
+import React from 'react';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
+import './blog.css';
+import { Article } from '../../components';
+
+const date = 'Sep 26, 2021';
+const text = 'GPT-3 and Open  AI is the future. Let us explore how it is?';
+
 const Blog = () => {
-  return <div>blog</div>;
+  return (
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>
+          A lot is happening, <br /> We are blogging about it.
+        </h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imgUrl={blog01} date={date} text={text} />
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+          <Article imgUrl={blog02} date={date} text={text} />
+          <Article imgUrl={blog03} date={date} text={text} />
+          <Article imgUrl={blog04} date={date} text={text} />
+          <Article imgUrl={blog05} date={date} text={text} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;

7ba52c947d8fe8779f8dcdfd6b60f05a4b30c7f4
michaWorku <mikias27worku@gmail.com>
2022-01-02 16:01:32 +0300

    footer component with style

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -4,7 +4,7 @@ const Article = ({ imgUrl, date, text }) => {
   return (
     <div className='gpt3__blog-container_article'>
       <div className='gpt3__blog-container_article-image'>
-        <img src={imgUrl} alt='blog image' />
+        <img src={imgUrl} alt='blog_image' />
       </div>
       <div className='gpt3__blog-container_article-content'>
         <div>
